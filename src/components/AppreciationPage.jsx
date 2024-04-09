ce30b02835104b644cce1e29873e5c5e1964fcfe
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2024-04-09 23:28:44 +0530

    Technical Assignment Alkye

--- src/components/AppreciationPage.jsx
+++ src/components/AppreciationPage.jsx
@@ -1,36 +1,32 @@
+import React from 'react';
 import Rectangle29 from '../assets/Rectangle 29.png';
 import Rectangle30 from '../assets/Rectangle 30.png';
 import Rectangle31 from '../assets/Rectangle 31.png';
 
 export const AppreciationPage = () => {
   return (
-    <div>
-      <div className="flex">
-        <div className="p-16">
-          <h1 className="font-oswald text-6xl">
+    <div className="container mx-auto py-12 px-4">
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-1/2">
+          <h1 className="font-oswald text-4xl md:text-6xl mb-8 md:mb-0">
             Employee{' '}
             <span className="text-orange-500">Appreciation Program</span>
           </h1>
-          <p className="pt-12">
-            Our Employee Appreciation Program is tailored to honor the ongoing
-            commitment and efforts of our long-serving team members. Our 5 Year
-            Club and 10 Year Club are special milestones within this program,
-            celebrating employees who have dedicated half a decade or a full
-            decade to our company mission. Each year, members of these clubs are
-            invited to annual events held at select, memorable locations,
-            reflecting our gratitude and recognition of their unwavering
-            dedication. It&apos;s our way of saying thank you and ensuring that
-            every significant chapter in our collective journey is celebrated
-            with the grandeur it deserves
+          <p className="text-lg md:text-xl">
+            Our Employee Appreciation Program honors the ongoing commitment and
+            efforts of our long-serving team members. Celebrating milestones of
+            5 and 10 years, members are invited to annual events held at select,
+            memorable locations, reflecting our gratitude and recognition of
+            their dedication.
           </p>
         </div>
-        <div className="">
-          <img src={Rectangle29} alt="" />
+        <div className="md:w-1/2 mt-8 md:mt-0">
+          <img src={Rectangle29} alt="Rectangle29" className="w-full" />
         </div>
       </div>
-      <div className="flex gap-12 px-12 py-6">
-        <img src={Rectangle30} alt="" />
-        <img src={Rectangle31} alt="" />
+      <div className="flex justify-center gap-8 mt-8">
+        <img src={Rectangle30} alt="Rectangle30" className="w-1/2" />
+        <img src={Rectangle31} alt="Rectangle31" className="w-1/2" />
       </div>
     </div>
   );
--- src/components/Connected.jsx
+++ src/components/Connected.jsx
@@ -1,15 +1,12 @@
+import React from 'react';
 import { RectangleConnected } from './RectangleConnected';
 import { StayConnected } from './StayConnected';
 
 export const Connected = () => {
   return (
     <>
-      <div>
-        <RectangleConnected />
-      </div>
-      <div className="">
-        <StayConnected />
-      </div>
+      <RectangleConnected />
+      <StayConnected />
     </>
   );
 };
